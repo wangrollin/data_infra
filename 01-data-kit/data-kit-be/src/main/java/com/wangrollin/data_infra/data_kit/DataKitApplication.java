ff18b2bf3a3c733fa9006b697fc147afeff366b2
wangrollin <wangrollin@foxmail.com>
2025-01-01 15:00:39 +0800

    init repo structure

---
+++ 01-data-kit/data-kit-be/src/main/java/com/wangrollin/data_infra/data_kit/DataKitApplication.java
@@ -0,0 +1,13 @@
+package com.wangrollin.data_infra.data_kit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataKitApplication.class, args);
+	}
+
+}
---
+++ 01-data-kit/data-kit-be/src/test/java/com/wangrollin/data_infra/data_kit/DataKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangrollin.data_infra.data_kit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
